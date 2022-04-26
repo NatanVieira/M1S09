ac4a9af57765fb92528ea0c2295408f34707551a
Natanael Vieira <natanaelvieira130@gmail.com>
2022-04-26 07:28:41 -0300

    M1S09 - Exercicio01

---
+++ exercicio01.js
@@ -0,0 +1,6 @@
+var exempleLet = 27;
+var exempleConst = 'Natanael Vieira';
+var exempleVar = "Ol\u00E1 meu nome \u00E9 ".concat(exempleConst, " e tenho ").concat(exempleLet, " anos");
+console.log(exempleLet);
+console.log(exempleConst);
+console.log(exempleVar);
---
+++ exercicio01.ts
@@ -0,0 +1,7 @@
+let exempleLet:number = 27;
+const exempleConst:string = 'Natanael Vieira';
+var exempleVar:string = `Olá meu nome é ${exempleConst} e tenho ${exempleLet} anos`;
+
+console.log(exempleLet);
+console.log(exempleConst);
+console.log(exempleVar);

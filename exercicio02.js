03b9cca23d570c20c6b9649ed704838dd71eb24b
Natanael Vieira <natanaelvieira130@gmail.com>
2022-04-26 07:35:19 -0300

    M1S09 - Exercicio 2

---
+++ exercicio02.js
@@ -0,0 +1,7 @@
+var soma = function (num1, num2) {
+    return num1 + num2;
+};
+{
+    var sum = soma(20, 8);
+    console.log(sum);
+}
---
+++ exercicio02.ts
@@ -0,0 +1,8 @@
+let soma = (num1:number, num2:number):number => {
+    return num1 + num2;
+}
+
+{
+    let sum:number = soma(20,8);
+    console.log(sum);
+}

1fe5aecfd6fce20934ee7269ee447b1bbaf4a9af
Natanael Vieira <natanaelvieira130@gmail.com>
2022-04-27 07:22:07 -0300

    M1S09 - Exercicio 03

---
+++ exercicio03.js
@@ -0,0 +1,8 @@
+var aluno;
+aluno = {
+    nome: 'Natanael',
+    matricula: 4751,
+    ativo: true,
+    cadeiras: ['JS', 'CSS']
+};
+console.log(aluno);
---
+++ exercicio03.ts
@@ -0,0 +1,15 @@
+let aluno: {
+    nome:string,
+    matricula:number,
+    ativo:boolean,
+    cadeiras:string[]
+}
+
+aluno = {
+    nome: 'Natanael',
+    matricula: 4751,
+    ativo: true,
+    cadeiras: ['JS','CSS']
+}
+
+console.log(aluno);

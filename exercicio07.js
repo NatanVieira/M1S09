a842bc245659fda95bae2d003491ba71f1d4ae28
Natanael Vieira <natanaelvieira130@gmail.com>
2022-04-29 07:13:34 -0300

    M1S09 - Exercicio07

---
+++ exercicio07.js
@@ -0,0 +1,33 @@
+var Aluno = /** @class */ (function () {
+    function Aluno() {
+    }
+    Aluno.prototype.cadastrar = function () {
+        console.log("Nome: ".concat(this.nome, " - Idade: ").concat(this.idade, " - Rua: ").concat(this.rua, " - Matricula: ").concat(this.matricula));
+        this.cadeiras.forEach(function (cadeira) {
+            console.log(cadeira);
+        });
+    };
+    return Aluno;
+}());
+var Funcionario = /** @class */ (function () {
+    function Funcionario() {
+    }
+    Funcionario.prototype.cadastrar = function () {
+        console.log("Nome: ".concat(this.nome, " - Idade: ").concat(this.idade, " - Rua: ").concat(this.rua, " - Identificador: ").concat(this.identificador, " - Setor: ").concat(this.setor));
+    };
+    return Funcionario;
+}());
+var aluno = new Aluno();
+var funcionario = new Funcionario();
+aluno.nome = 'Natanael';
+aluno.idade = 27;
+aluno.matricula = 4751;
+aluno.rua = 'Vicente Calderari';
+aluno.cadeiras = ['Angular', 'Typescript'];
+funcionario.nome = 'Paulo';
+funcionario.idade = 53;
+funcionario.identificador = 5555;
+funcionario.rua = 'Tucano';
+funcionario.setor = 'Manutenção';
+aluno.cadastrar();
+funcionario.cadastrar();
---
+++ exercicio07.ts
@@ -0,0 +1,53 @@
+interface Pessoa {
+    nome: string;
+    idade: number;
+    rua: string;
+
+    cadastrar();
+}
+
+class Aluno implements Pessoa{
+    nome: string;
+    idade: number;
+    rua: string;
+    matricula: number;
+    cadeiras: string[];
+
+    cadastrar(){
+        console.log(`Nome: ${this.nome} - Idade: ${this.idade} - Rua: ${this.rua} - Matricula: ${this.matricula}`);
+        this.cadeiras.forEach(cadeira => {
+            console.log(cadeira);
+        });
+    }
+}
+
+class Funcionario implements Pessoa{
+    nome: string;
+    idade: number;
+    rua: string;
+    identificador: number;
+    setor: string;
+
+    cadastrar(){
+        console.log(`Nome: ${this.nome} - Idade: ${this.idade} - Rua: ${this.rua} - Identificador: ${this.identificador} - Setor: ${this.setor}`);
+    }
+}
+
+
+let aluno = new Aluno();
+let funcionario =  new Funcionario();
+
+aluno.nome = 'Natanael';
+aluno.idade = 27;
+aluno.matricula = 4751;
+aluno.rua = 'Vicente Calderari';
+aluno.cadeiras = ['Angular','Typescript'];
+
+funcionario.nome = 'Paulo';
+funcionario.idade = 53;
+funcionario.identificador = 5555;
+funcionario.rua = 'Tucano';
+funcionario.setor = 'Manutenção';
+
+aluno.cadastrar();
+funcionario.cadastrar();
